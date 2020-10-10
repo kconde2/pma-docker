ee0ba4ad685b4c39a85bd7adfc8152053dd3ebef
Kaba CONDE <kabaconde15@gmail.com>
2020-10-10 04:55:06 +0200

    Init project

---
+++ config.user.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+// https://docs.phpmyadmin.net/fr/latest/config.html
+$cfg['MaxNavigationItems'] =  300;
+
+$cfg['MaxRows'] = 100;
+
+$cfg['ThemeDefault'] = 'original';
+
+$cfg['Console']['DarkTheme'] = true;
+
+$cfg['Console']['Height'] = 120;
